d20805706ebbcc5107383782552f64da062c24b8
Radi Cho <radicho123@gmail.com>
2025-02-15 01:37:18 -0800

    Update model device assignment.

--- kernel_api.py
+++ kernel_api.py
@@ -20,7 +20,8 @@ kernel_module = load_inline(
 def time_execution_with_cuda_event():
     from sample.kernel import Model, get_inputs
     device = torch.cuda.current_device()
-    torch_model = Model(device=device)
+    torch_model = Model()
+    torch_model.to(device=device)
     inputs = [inp.to(device=device) for inp in get_inputs()]
 
     elapsed_times_torch = []
